7388983aef63d7e56d33b10ceec379b34726bf5c
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-09-19 00:57:02 +0900

    Pixel Art Maker First Submission

--- IntermediateJavaScrpt/project/PixelArtMaker/designs.js
+++ IntermediateJavaScrpt/project/PixelArtMaker/designs.js
@@ -5,12 +5,28 @@
 
 function makeGrid() {
     // Your code goes here!
-    let inputHeight = $('#input_height').val();
-    let inputWidth = $('#input_width').val();
+    let inputHeight = $("#input_height").val();
+    let inputWidth = $("#input_width").val();
+    let tableTag = $("#pixel_canvas")
+    tableTag.empty();
+    for (let row=inputHeight; row--; row>=0){
+        tableTag.append("<tr class=table_created></tr>");
+    }
+    let tableRowTag = $("tr").empty();
+    for (let column=inputWidth; column--; column>=0){
+        tableRowTag.append("<td class='table_column'></td>");
+    }
     console.log(inputHeight + inputWidth);
 };
 
-$('#sizePicker').submit(function(event){
+$("#sizePicker").submit(function(event){
     event.preventDefault();
     makeGrid();
 });
+
+$(document).on("click",".table_column", function(){
+    let bgColor = $(colorPicker).val();
+    let tableColumn = $(this);
+    console.log(tableColumn);
+    tableColumn.css({"background-color": bgColor});
+})
