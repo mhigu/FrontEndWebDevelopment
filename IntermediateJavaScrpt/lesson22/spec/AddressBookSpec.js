2f06a2e069330278da174ffd6b24d13189e578aa
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-10-17 21:19:29 +0900

    Lesson22 Initial Commit (Jasmin Test )

---
+++ IntermediateJavaScrpt/lesson22/spec/AddressBookSpec.js
@@ -0,0 +1,22 @@
+describe('Address Book', function(){
+
+    var addressBook, thisContact;
+
+    beforeEach(function(){
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
+
+    it ('should be able to add contact', function() {
+        addressBook.addContact(thisContact);
+
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+
+    it ('should be able to delete contact', function() {
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
+});
