beebd4341663993f86fa4d1b454b5aff91aee15e
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-10-17 14:30:56 +0900

    Finish Implementing of ClassicArcadeGameClone

--- IntermediateJavaScrpt/project/ClassicArcadeGameClone/js/app.js
+++ IntermediateJavaScrpt/project/ClassicArcadeGameClone/js/app.js
@@ -1,58 +1,125 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 0;
-    this.y = 50;
-    this.speed = 100;
+    this.x = x; // x axis of enemy
+    this.y = y; // y axis of enemy
+    this.speed = speed; // spped of eneymy
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+ * @description Update the enemy's position, required method for game
+ * @param dt a time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
-    // this.y += this.speed * dt;
+    var yAxis = [72, 154, 236];
+    // select row randomly from yAxis(the player will be somewhere).
+    if (this.x > 490){
+        this.y = yAxis.sort(function() { return 0.5 - Math.random();}).pop();
+    }
+    this.x = (this.x + (this.speed * dt))%550;
+    this.handleCollision(player, this);
+    
 };
 
-// Draw the enemy on the screen, required method for game
+/**
+ * @description This function handle collision.
+ * 1. check collision happened.
+ * 2. if true, return player to first position.
+ * 3. and increment/update lose counter.
+ * @param p player object
+ * @param e enemy object
+ */
+Enemy.prototype.handleCollision = function(p, e) {
+    var margin = 20;
+    if (p.y===e.y && (p.x-margin < e.x && e.x < p.x)){
+        p.x = 200;
+        p.y = 400;
+        p.lose += 1;
+        $(".lose-counter").text(p.lose);
+    }
+};
+
+/**
+ * @description Draw the enemy on the screen, required method for game.
+ */ 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 var Player = function() {
-    this.x = 200;
-    this.y = 400;
+    this.x = 200; // x axis of player
+    this.y = 400; // y axis of player
     this.sprite = 'images/char-boy.png';
+    this.win = 0; // win count
+    this.lose = 0;  // lose count
 };
 
 // This class requires an update(), render() and
+/**
+ * @description This function do nothing special. Just stay same position.
+ */
 Player.prototype.update = function() {
     this.x = this.x;
     this.y = this.y;
 };
 
+/**
+ * @description Draw the player on the screen, required method for game
+ */
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// a handleInput() method.
-// direction pattern is ['left', 'up', 'right', 'down'].
+/**
+ * @description a handleInput() method. 
+ * @param direction ['left', 'up', 'right', 'down']
+ */
 Player.prototype.handleInput = function(direction) {
-    console.log(`inside handle input method.. ${direction}`);
+
+    if (direction==='left' && this.x>0){
+        this.x += -100;
+    } else if (direction==='up' && this.y>-10){
+        this.y += -82;
+        this.refresh(this.x, this.y);
+    } else if (direction==='right' && this.x<400){
+        this.x += 100;
+    } else if (direction==='down' && this.y<400){
+        this.y += 82;
+    } 
+};
+
+/**
+ * @description if player reach water refresh player position.
+ * @param px player x position
+ * @param py player y position
+ */
+Player.prototype.refresh = function(px, py){
+    if (py<31){
+        this.x = 200;
+        this.y = 400;
+        this.win += 1;
+        $(".win-counter").text(this.win);
+    }
 };
 
 // Now instantiate your objects.
-var enemy1 = new Enemy();
+var enemy1 = new Enemy(0, 72, 300);
+var enemy2 = new Enemy(0, 72, 500);
+var enemy3 = new Enemy(0, 154, 100);
+var enemy4 = new Enemy(0, 154, 50);
+var enemy5 = new Enemy(0, 236, 400);
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [enemy1];
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
+// var allEnemies = [enemy1];
 // Place the player object in a variable called player
 var player = new Player();
 
