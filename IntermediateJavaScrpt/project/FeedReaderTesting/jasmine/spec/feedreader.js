107d209b30ace0c775916a945bbb131cc37f2bd6
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-10-18 16:31:34 +0900

    Write Test Suite for "Initial Entries"
    [x] Write a test suite named "Initial Entries".
    [x] Write a test that ensures when the loadFeed function is called and
    completes its work, there is at least a single .entry element within
    the .feed container.

--- IntermediateJavaScrpt/project/FeedReaderTesting/jasmine/spec/feedreader.js
+++ IntermediateJavaScrpt/project/FeedReaderTesting/jasmine/spec/feedreader.js
@@ -94,6 +94,7 @@ $(function() {
 
     
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -101,11 +102,22 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                done();
+            });
+        });
+        
+        it ('There is at least a single .entry', function(done){
+            expect($('.feed').children().length).toBeGreaterThan(1);
+            done();
+        });
+    });
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
-         */
+         */    
 }());
