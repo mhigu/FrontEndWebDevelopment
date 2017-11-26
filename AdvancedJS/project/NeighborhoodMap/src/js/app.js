f27ff7a75c6be1995a6dfb54ea6447934514d2ee
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-11-26 22:51:31 +0900

    Refactor Map stuff

--- AdvancedJS/project/NeighborhoodMap/src/js/app.js
+++ AdvancedJS/project/NeighborhoodMap/src/js/app.js
@@ -13,11 +13,12 @@ function initMap() {
   let locations = [
     {title: 'hoge1', location: {lat: -34.397, lng: 150.644}},
     {title: 'hoge2', location: {lat: -34.387, lng: 150.544}},
-    // {title: 'hoge3', location: {lat: -33.397, lng: 150.644}},
-    // {title: 'hoge4', location: {lat: -32.397, lng: 149.644}},
+    {title: 'hoge3', location: {lat: -33.397, lng: 150.644}},
+    {title: 'hoge4', location: {lat: -33.397, lng: 149.644}},
   ]
 
   let largeInfoWindow = new google.maps.InfoWindow();
+  let bounds = new google.maps.LatLngBounds();
 
   for (let i = 0; i < locations.length; i++){
     let position = locations[i].location;
@@ -34,22 +35,22 @@ function initMap() {
     marker.addListener('click', function(){
       populateInfoWindow(this, largeInfoWindow);
     });
+    bounds.extend(markers[i].position);
   }
-
-  function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
-      infowindow.open(map, marker);
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick',function(){
-        infowindow.setMarker = null;
-      });
-    }
-  }
+  map.fitBounds(bounds);
 }
 
-
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick',function(){
+      infowindow.setMarker = null;
+    });
+  }
+}
 
 // ko.applyBindings(new ClickCounterViewModel());
