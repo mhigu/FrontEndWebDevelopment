90b3cbbb450905bbb9f139364f28e753d53807e1
Manabu Higuchi <danbouruno.ie+github@gmail.com>
2017-12-06 22:44:56 +0900

    Create pop up window

--- AdvancedJS/project/NeighborhoodMap/src/js/app.js
+++ AdvancedJS/project/NeighborhoodMap/src/js/app.js
@@ -1,5 +1,7 @@
-const markers = [];
 let map;
+
+// Create a new blank array for all the listing markers.
+const markers = [];
 
 // Create placemarkers array to use in multiple functions to have control
 // over the number of places that show.
@@ -13,58 +15,58 @@ function initMap() {
       stylers: [
         { color: '#19a0d8' }
       ]
-    },{
+    }, {
       featureType: 'administrative',
       elementType: 'labels.text.stroke',
       stylers: [
         { color: '#ffffff' },
         { weight: 6 }
       ]
-    },{
+    }, {
       featureType: 'administrative',
       elementType: 'labels.text.fill',
       stylers: [
         { color: '#e85113' }
       ]
-    },{
+    }, {
       featureType: 'road.highway',
       elementType: 'geometry.stroke',
       stylers: [
         { color: '#efe9e4' },
         { lightness: -40 }
       ]
-    },{
+    }, {
       featureType: 'transit.station',
       stylers: [
         { weight: 9 },
         { hue: '#e85113' }
       ]
-    },{
+    }, {
       featureType: 'road.highway',
       elementType: 'labels.icon',
       stylers: [
         { visibility: 'off' }
       ]
-    },{
+    }, {
       featureType: 'water',
       elementType: 'labels.text.stroke',
       stylers: [
         { lightness: 100 }
       ]
-    },{
+    }, {
       featureType: 'water',
       elementType: 'labels.text.fill',
       stylers: [
         { lightness: -100 }
       ]
-    },{
+    }, {
       featureType: 'poi',
       elementType: 'geometry',
       stylers: [
         { visibility: 'on' },
         { color: '#f0e4d3' }
       ]
-    },{
+    }, {
       featureType: 'road.highway',
       elementType: 'geometry.fill',
       stylers: [
@@ -73,47 +75,39 @@ function initMap() {
       ]
     }
   ];
-
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: { lat: 40.7413549, lng: -73.9980244 },
     zoom: 13,
     styles: styles,
     mapTypeControl: false
   });
-
   // This autocomplete is for use in the geocoder entry box.
   const zoomAutocomplete = new google.maps.places.Autocomplete(
     document.getElementById('zoom-to-area-text'));
-  //Bias the boundaries within the map for the zoom to area text.
+  // Bias the boundaries within the map for the zoom to area text.
   zoomAutocomplete.bindTo('bounds', map);
-
-  // Create a searchbox in order to execute a place search
+  // Create a searchbox in order to execute a places search
   const searchBox = new google.maps.places.SearchBox(
     document.getElementById('places-search'));
+  // Bias the searchbox to within the bounds of the map.
   searchBox.setBounds(map.getBounds());
-
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
   const locations = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    { title: 'Park Ave Penthouse', location: { lat: 40.7713024, lng: -73.9632393 } },
+    { title: 'Chelsea Loft', location: { lat: 40.7444883, lng: -73.9949465 } },
+    { title: 'Union Square Open Floor Plan', location: { lat: 40.7347062, lng: -73.9895759 } },
+    { title: 'East Village Hip Studio', location: { lat: 40.7281777, lng: -73.984377 } },
+    { title: 'TriBeCa Artsy Bachelor Pad', location: { lat: 40.7195264, lng: -74.0089934 } },
+    { title: 'Chinatown Homey Space', location: { lat: 40.7180628, lng: -73.9961237 } }
   ];
-
   const largeInfowindow = new google.maps.InfoWindow();
-
-
   // Style the markers a bit. This will be our listing marker icon.
   const defaultIcon = makeMarkerIcon('0091ff');
-
   // Create a "highlighted location" marker color for when the user
   // mouses over the marker.
   const highlightedIcon = makeMarkerIcon('FFFF24');
-
   // The following group uses the location array to create an array of markers on initialize.
   for (let i = 0; i < locations.length; i++) {
     // Get the position from the location array.
@@ -130,35 +124,33 @@ function initMap() {
     // Push the marker to our array of markers.
     markers.push(marker);
     // Create an onclick event to open the large infowindow at each marker.
-    marker.addListener('click', function() {
+    marker.addListener('click', function () {
       populateInfoWindow(this, largeInfowindow);
     });
     // Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
-    marker.addListener('mouseover', function() {
+    marker.addListener('mouseover', function () {
       this.setIcon(highlightedIcon);
     });
-    marker.addListener('mouseout', function() {
+    marker.addListener('mouseout', function () {
       this.setIcon(defaultIcon);
     });
   }
-
   document.getElementById('show-listings').addEventListener('click', showListings);
-  document.getElementById('hide-listings').addEventListener('click', hideMarkers);
-  // google.maps.event.addDomListener(window, 'resize', initialize);
-  document.getElementById('zoom-to-area').addEventListener('click', function() {
+  document.getElementById('hide-listings').addEventListener('click', function () {
+    hideMarkers(markers);
+  });
+  document.getElementById('zoom-to-area').addEventListener('click', function () {
     zoomToArea();
   });
   // Listen for the event fired when the user selects a prediction from the
   // picklist and retrieve more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', function () {
     searchBoxPlaces(this);
   });
-
   // Listen for the event fired when the user selects a prediction and clicks
   // "go" more details for that place.
   document.getElementById('go-places').addEventListener('click', textSearchPlaces);
-        
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -171,7 +163,7 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.setContent('');
     infowindow.marker = marker;
     // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
+    infowindow.addListener('closeclick', function () {
       infowindow.marker = null;
     });
     const streetViewService = new google.maps.StreetViewService();
@@ -184,14 +176,14 @@ function populateInfoWindow(marker, infowindow) {
         const nearStreetViewLocation = data.location.latLng;
         const heading = google.maps.geometry.spherical.computeHeading(
           nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          const panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
+        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+        const panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 30
+          }
+        };
         const panorama = new google.maps.StreetViewPanorama(
           document.getElementById('pano'), panoramaOptions);
       } else {
@@ -206,7 +198,6 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.open(map, marker);
   }
 }
-
 // This function will loop through the markers array and display them all.
 function showListings() {
   const bounds = new google.maps.LatLngBounds();
@@ -217,30 +208,23 @@ function showListings() {
   }
   map.fitBounds(bounds);
 }
-
 // This function will loop through the listings and hide them all.
-// function hideListings() {
-//   for (let i = 0; i < markers.length; i++) {
-//     markers[i].setMap(null);
-//   }
-// }
-function hideMarkers(markers){
-  for (let i = 0; i < markers.length; i++){
+function hideMarkers(markers) {
+  for (let i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
 }
-
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
   const markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
     '|40|_|%E2%80%A2',
     new google.maps.Size(21, 34),
     new google.maps.Point(0, 0),
     new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
+    new google.maps.Size(21, 34));
   return markerImage;
 }
 
@@ -259,47 +243,52 @@ function zoomToArea() {
     // Geocode the address/area entered to get the center. Then, center the map
     // on it and zoom in
     geocoder.geocode(
-      { address: address,
-        // componentRestrictions: {locality: 'New York'}
-      }, function(results, status) {
+      {
+        address: address,
+        // componentRestrictions: { locality: 'New York' }
+      }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           map.setCenter(results[0].geometry.location);
           map.setZoom(15);
         } else {
           window.alert('We could not find that location - try entering a more' +
-              ' specific place.');
+            ' specific place.');
         }
       });
   }
 }
 
-function searchBoxPlaces(searchBox){
+// This function fires when the user selects a searchbox picklist item.
+// It will do a nearby search using the selected query string or place.
+function searchBoxPlaces(searchBox) {
   hideMarkers(placeMarkers);
   const places = searchBox.getPlaces();
-  // For each place, getthe icon, name and location.
-  createMarkersForPlaces(places);
-  if (places.length == 0){
+  if (places.length === 0) {
     window.alert('We did not find any places matching that search!');
+  } else {
+    // For each place, get the icon, name and location.
+    createMarkersForPlaces(places);
   }
 }
-
-function textSearchPlaces(){
+// This function firest when the user select "go" on the places search.
+// It will do a nearby search using the entered query string or place.
+function textSearchPlaces() {
   const bounds = map.getBounds();
   hideMarkers(placeMarkers);
   const placesService = new google.maps.places.PlacesService(map);
   placesService.textSearch({
     query: document.getElementById('places-search').value,
     bounds: bounds
-  }, function(results, status){
-    if (status === google.maps.places.PlacesServiceStatus.OK){
+  }, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
       createMarkersForPlaces(results);
     }
   });
 }
-
-function createMarkersForPlaces(places){
+// This function creates markers for each place found in either places search.
+function createMarkersForPlaces(places) {
   const bounds = new google.maps.LatLngBounds();
-  for (let i = 0; i < places.length; i++){
+  for (let i = 0; i < places.length; i++) {
     const place = places[i];
     const icon = {
       url: place.icon,
@@ -314,7 +303,18 @@ function createMarkersForPlaces(places){
       icon: icon,
       title: place.name,
       position: place.geometry.location,
-      id: place.id
+      id: place.place_id
+    });
+    // Create a single infowindow to be used with the place details information
+    // so that only one is open at once.
+    let placeInfoWindow = new google.maps.InfoWindow();
+    // If a marker is clicked, do a place details search on it in the next function.
+    marker.addListener('click', function () {
+      if (placeInfoWindow.marker === this) {
+        console.log("This infowindow already is on this marker!");
+      } else {
+        getPlacesDetails(this, placeInfoWindow);
+      }
     });
     placeMarkers.push(marker);
     if (place.geometry.viewport) {
@@ -325,6 +325,51 @@ function createMarkersForPlaces(places){
     }
   }
   map.fitBounds(bounds);
+}
+// This is the PLACE DETAILS search - it's the most detailed so it's only
+// executed when a marker is selected, indicating the user wants more
+// details about that place.
+function getPlacesDetails(marker, infowindow) {
+  const service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: marker.id
+  }, function (place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      // Set the marker property on this infowindow so it isn't created again.
+      infowindow.marker = marker;
+      let innerHTML = '<div>';
+      if (place.name) {
+        innerHTML += '<strong>' + place.name + '</strong>';
+      }
+      if (place.formatted_address) {
+        innerHTML += '<br>' + place.formatted_address;
+      }
+      if (place.formatted_phone_number) {
+        innerHTML += '<br>' + place.formatted_phone_number;
+      }
+      if (place.opening_hours) {
+        innerHTML += '<br><br><strong>Hours:</strong><br>' +
+          place.opening_hours.weekday_text[0] + '<br>' +
+          place.opening_hours.weekday_text[1] + '<br>' +
+          place.opening_hours.weekday_text[2] + '<br>' +
+          place.opening_hours.weekday_text[3] + '<br>' +
+          place.opening_hours.weekday_text[4] + '<br>' +
+          place.opening_hours.weekday_text[5] + '<br>' +
+          place.opening_hours.weekday_text[6];
+      }
+      if (place.photos) {
+        innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+          { maxHeight: 100, maxWidth: 200 }) + '">';
+      }
+      innerHTML += '</div>';
+      infowindow.setContent(innerHTML);
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function () {
+        infowindow.marker = null;
+      });
+    }
+  });
 }
 
 // ko.applyBindings(new ClickCounterViewModel());
